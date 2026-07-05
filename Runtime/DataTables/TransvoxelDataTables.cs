40cb06b8bf69808a40f11fe805156df26d7582f1
Roman Likhadievski <279574612+reromanlee@users.noreply.github.com>
2026-07-05 06:39:47 +0300

    generate meta files

---
+++ Runtime/DataTables/TransvoxelDataTables.cs
@@ -0,0 +1,8 @@
+// ==============================================
+//
+// Transvoxel Algorithm Data Tables
+// https://transvoxel.org/
+//
+// Copyright 2009 by Eric Lengyel
+//
+// ==============================================
