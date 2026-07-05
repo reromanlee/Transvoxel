6befe41f1315c23cfbcf6df6aaa4148049baa8e8
Roman Likhadievski <279574612+reromanlee@users.noreply.github.com>
2026-07-05 16:14:55 +0300

    create: blank assemblies

--- Runtime/DataTables/TransvoxelDataTables.cs
+++
@@ -1,8 +0,0 @@
-// ==============================================
-//
-// Transvoxel Algorithm Data Tables
-// https://transvoxel.org/
-//
-// Copyright 2009 by Eric Lengyel
-//
-// ==============================================
